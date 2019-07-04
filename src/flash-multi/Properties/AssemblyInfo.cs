8f7ab0b8fc654d36b8ad5adc84d6fe05f7b386a5
Ben Lye <ben@lye.co.nz>
2019-07-04 10:23:21 +0100

    Visual Studio project updates

--- src/flash-multi/Properties/AssemblyInfo.cs
+++ src/flash-multi/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("flash-multi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Multiprotocol TX module firmware updater")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ben Lye")]
 [assembly: AssemblyProduct("flash-multi")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
