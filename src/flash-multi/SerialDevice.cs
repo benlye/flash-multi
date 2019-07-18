30ffb0fcbfde25d448e8e3fd9d03e731abc8e31e
Ben Lye <ben@lye.co.nz>
2019-07-18 16:44:52 +0100

    Update SerialDevice.cs

--- src/flash-multi/SerialDevice.cs
+++ src/flash-multi/SerialDevice.cs
@@ -20,7 +20,6 @@
 
 namespace Flash_Multi
 {
-    using System;
     using System.Threading.Tasks;
     using System.Windows.Forms;
 
@@ -37,28 +36,59 @@ namespace Flash_Multi
         /// <param name="comPort">The COM port where the serial device can be found.</param>
         public static async void WriteFlash(FlashMulti flashMulti, string fileName, string comPort)
         {
+            // Path to the flashing tool, stm32flash.exe
             string command = ".\\tools\\stm32flash.exe";
+
+            // Path to the bootloader file
             string bootLoaderPath = ".\\bootloaders\\StmMulti4in1.bin";
+
+            // Baud rate for serial flash commands
+            int serialBaud = 115200;
+
+            // Arguments for the command line - will vary at each step of the process
             string commandArgs;
 
+            // Variable to keep the return code from executed commands
             int returnCode = -1;
+
+            // First step in flash process
             int flashStep = 1;
+
+            // Total number of steps in flash process
             int flashSteps = 2;
 
+            // Page in the STM32 flash memory where we will begin writing
             int flashStart = 0;
+
+            // Address where we will start execution after flashing
             string executionAddress = "0x8000000";
 
             if (flashMulti.writeBootloader_Yes.Checked)
             {
+                // Increase the total number of steps
                 flashSteps = 3;
+
+                // The bootloader occupies the first 8 pages (0-7), so start writing after it
+                flashStart = 8;
+
+                // Start execution at the Multiprotocol code rather than the bootloader
+                executionAddress = "0x8002000";
             }
 
-            flashMulti.AppendLog("Starting Multimodule update\r\n");
+            // Write to the log
+            flashMulti.AppendLog("Starting Multimodule update via serial\r\n");
 
-            // Erase
+            // Erase the flash
             flashMulti.AppendLog($"[{flashStep}/{flashSteps}] Erasing flash memory...");
-            commandArgs = string.Format("-o -S 0x8000000:129024 -b 115200 {0}", comPort);
+
+            // Set the stm32flash.exe command line arguments for erasing
+            // We preserve the last 2KB of flash, which is where the EEPROM data lives.
+            commandArgs = $"-o -S 0x8000000:129024 -b {serialBaud} {comPort}";
+
+            // Run the erase command asynchronously and wait for it to finish
             await Task.Run(() => { returnCode = RunCommand.Run(flashMulti, command, commandArgs); });
+
+            // Show an error message if the erase command failed for any reason
             if (returnCode != 0)
             {
                 flashMulti.EnableControls(true);
@@ -69,13 +99,20 @@ namespace Flash_Multi
 
             flashMulti.AppendLog(" done\r\n");
 
+            // Write the bootloader, if it was selected
             if (flashMulti.writeBootloader_Yes.Checked)
             {
-                // Flash bootloader
+                // Increment the step counter and write to the log
                 flashStep++;
                 flashMulti.AppendLog($"[{flashStep}/{flashSteps}] Writing bootloader...");
-                commandArgs = $"-v -e 0 -g {executionAddress} -b 115200 -w \"{bootLoaderPath}\" {comPort}";
+
+                // Prepare the command line arguments for writing the bootloader
+                commandArgs = $"-v -e 0 -g {executionAddress} -b {serialBaud} -w \"{bootLoaderPath}\" {comPort}";
+
+                // Run the write command asynchronously and wait for it to finish
                 await Task.Run(() => { returnCode = RunCommand.Run(flashMulti, command, commandArgs); });
+
+                // Show an error message if the command failed for any reason
                 if (returnCode != 0)
                 {
                     flashMulti.EnableControls(true);
@@ -85,17 +122,19 @@ namespace Flash_Multi
                 }
 
                 flashMulti.AppendLog(" done\r\n");
-
-                // Set the flash address for a flash with bootloader
-                flashStart = 8;
-                executionAddress = "0x8002000";
             }
 
-            // Flash firmware
+            // Increment the step counter and write to the log
             flashStep++;
             flashMulti.AppendLog($"[{flashStep}/{flashSteps}] Writing Multimodule firmware...");
-            commandArgs = $"-v -s {flashStart} -e 0 -g {executionAddress} -b 115200 -w \"{fileName}\" {comPort}";
+
+            // Prepare the command line arguments for writing the firmware
+            commandArgs = $"-v -s {flashStart} -e 0 -g {executionAddress} -b {serialBaud} -w \"{fileName}\" {comPort}";
+
+            // Run the write command asynchronously and wait for it to finish
             await Task.Run(() => { returnCode = RunCommand.Run(flashMulti, command, commandArgs); });
+
+            // Show an error message if the command failed for any reason
             if (returnCode != 0)
             {
                 flashMulti.EnableControls(true);
@@ -104,10 +143,14 @@ namespace Flash_Multi
                 return;
             }
 
+            // Write a success message to the log
             flashMulti.AppendLog(" done\r\n");
             flashMulti.AppendLog("\r\nMultimodule updated sucessfully");
 
+            // Show a success message box
             MessageBox.Show("Multimodule updated sucessfully.", "Firmware Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Re-enable the form controls
             flashMulti.EnableControls(true);
         }
     }
