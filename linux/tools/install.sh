47ccc8ef16c63b7c2273f692bab48af3523ce4b2
Ben Lye <ben@lye.co.nz>
2019-08-30 19:18:40 +0100

    Update install.sh

--- linux/tools/install.sh
+++ linux/tools/install.sh
@@ -7,8 +7,9 @@ if sudo [ -w /etc/udev/rules.d ]; then
     sudo chmod 644 /etc/udev/rules.d/45-maple.rules
     echo "Reloading udev rules"
     sudo udevadm control --reload-rules
-    echo "Adding current user to dialout group"
-    sudo adduser $USER dialout
+    echo "Adding current user to dialout and plugdev groups"
+    sudo usermod -a -G plugdev $USER
+    sudo usermod -a -G dialout $USER
 else
     echo "Couldn't copy to /etc/udev/rules.d/; Did you run the script as root? Does your distribution of Linux include udev?"
 fi
